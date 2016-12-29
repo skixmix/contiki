5a66cb2758bea8a8da6aface806cb9324d048753
BigMikes <giulio.micheloni@gmail.com>
2016-12-29 12:05:34 +0100

    Added explicit values to enumerations. It is needed to interpret correctly the Cbor payload sent by the Controller.

--- core/net/sdn/flowtable.h
+++ core/net/sdn/flowtable.h
@@ -45,46 +45,46 @@ typedef struct bytes16{
 } bytes16_t;
 
 typedef enum {
-    FORWARD,
-    DROP,
-    MODIFY,
-    DECREMENT,
-    INCREMENT,
-    CONTINUE,
-    TO_UPPER_L
+    FORWARD = 1,
+    DROP = 2,
+    MODIFY = 3,
+    DECREMENT = 4,
+    INCREMENT = 5,
+    CONTINUE = 6,
+    TO_UPPER_L = 7
 } action_type_t;
 
 typedef enum {
-    NO_FIELD,
-    LINK_SRC_ADDR,          //802.15.4 source address
-    LINK_DST_ADDR,          //802.15.4 destination address
-    MH_SRC_ADDR,            //Mesh Header Originator Address field
-    MH_DST_ADDR,            //Mesh Header Final Address field
-    MH_HL,                  //Mesh Header Hop Limit field
-    SICSLO_DISPATCH,        //6LoWPAN Dispatch Type field(s)
-    SICSLO_BRDCST_HDR,      //6LoWPAN Broadcast Header
-    SICSLO_FRAG1_HDR,       //6LoWPAN Fragmentation Header (first fragment)
-    SICSLO_FRAGN_HDR,       //6LoWPAN Fragmentation Header (subsequent fragments)
-    SICSLO_IPHC,            //6LoWPAN IPv6 Header Compression
-    SICSLO_NHC,             //6LoWPAN IPv6 Next Header Compression
-    SICSLO_IPV6,            //6LoWPAN IPv6 Uncompressed IPv6 addresses
-    IP_SRC_ADDR,            //IPv6 packet Source Address field
-    IP_DST_ADDR,            //IPv6 packet Destination Address field
-    IP_HL,                  //IPv6 packet Hop Limit field
-    IP_TC,                  //IPv6 packet Traffic Class field
-    IP_FL,                  //IPv6 packet Flow Label field
-    IP_NH,                  //IPv6 packet Next Header field
-    IP_PAYLOAD,             //Payload of the IPv6 packet
-    NODE_STATE              //State array
+    NO_FIELD = 1,
+    LINK_SRC_ADDR = 2,          //802.15.4 source address
+    LINK_DST_ADDR = 3,          //802.15.4 destination address
+    MH_SRC_ADDR = 4,            //Mesh Header Originator Address field
+    MH_DST_ADDR = 5,            //Mesh Header Final Address field
+    MH_HL = 6,                  //Mesh Header Hop Limit field
+    SICSLO_DISPATCH = 7,        //6LoWPAN Dispatch Type field(s)
+    SICSLO_BRDCST_HDR = 8,      //6LoWPAN Broadcast Header
+    SICSLO_FRAG1_HDR = 9,       //6LoWPAN Fragmentation Header (first fragment)
+    SICSLO_FRAGN_HDR = 10,       //6LoWPAN Fragmentation Header (subsequent fragments)
+    SICSLO_IPHC = 11,            //6LoWPAN IPv6 Header Compression
+    SICSLO_NHC = 12,             //6LoWPAN IPv6 Next Header Compression
+    SICSLO_IPV6 = 13,            //6LoWPAN IPv6 Uncompressed IPv6 addresses
+    IP_SRC_ADDR = 14,            //IPv6 packet Source Address field
+    IP_DST_ADDR = 15,            //IPv6 packet Destination Address field
+    IP_HL = 16,                  //IPv6 packet Hop Limit field
+    IP_TC = 17,                  //IPv6 packet Traffic Class field
+    IP_FL = 18,                  //IPv6 packet Flow Label field
+    IP_NH = 19,                  //IPv6 packet Next Header field
+    IP_PAYLOAD = 20,             //Payload of the IPv6 packet
+    NODE_STATE = 21              //State array
 } field_t;
 
 typedef enum {
-    EQUAL,
-    NOT_EQUAL,
-    GREATER,
-    LESS,
-    GREATER_OR_EQUAL,
-    LESS_OR_EQUAL
+    EQUAL = 1,
+    NOT_EQUAL = 2,
+    GREATER = 3,
+    LESS = 4,
+    GREATER_OR_EQUAL = 5,
+    LESS_OR_EQUAL = 6
 } operator_t;
 
 typedef union {
