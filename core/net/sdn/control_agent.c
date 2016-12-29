990bdacecb73d5acefae26ccbdb06686f6daf886
BigMikes <giulio.micheloni@gmail.com>
2016-12-29 15:39:59 +0100

    Now to store the packet that has generated a table miss the queuebuf module is being used. This should avoid waste of RAM.

--- core/net/sdn/control_agent.c
+++ core/net/sdn/control_agent.c
@@ -32,10 +32,7 @@ uip_ipaddr_t server_ipaddr;
 
 static uint8_t payload[MAX_DIM_PAYLOAD];
 static char uri_query[50];
-
-const uint8_t msg[] = {0x84, 0x01, 0x18, 0x5f, 0x18, 0x2a, 0xa2, \
-                    0x48, 0x02, 0x00, 0x01, 0x00, 0x00, 0x40, 0x00, 0x01, 0x82, 0x18, 0x3e, 0x18, 0x3a, \
-                    0x48, 0x04, 0x02, 0x00, 0x00, 0x05, 0x00, 0x00, 0x02, 0x82, 0x18, 0x3e, 0x18, 0x3a};
+struct queuebuf* q;
 
 void start_request(void){
     process_poll(&coap_client_process);
@@ -209,26 +206,33 @@ static void callback_decrement_ttl(void *ptr){
 void handleTableMiss(linkaddr_t* L2_receiver, linkaddr_t* L2_sender, uint8_t* ptr_to_pkt, uint16_t pkt_dim){
     request_t* req = NULL;
     uint8_t payload_dim = 0;
+    
+    if(queuebuf_numfree() == 0)
+        return;
+    q = queuebuf_new_from_packetbuf();
+    if(q == NULL)
+        return;
     req = add_request();
-    if(req != NULL){
-        //Set type of message: CON and POST
-        coap_init_message(&req->req_packet, COAP_TYPE_CON, COAP_POST, 0);
-        //Set the target resource 
-        coap_set_header_uri_path(&req->req_packet, "Flow_engine");
-        //Set the query parameter: "?type=all_packet&tx_mac=<L2 sender's mac address>&rx_mac=<L2 receiver's mac address>"
-        //URI_QUERY_ALL_PACKET(uri_query, L2_sender, L2_receiver);
-        URI_QUERY_ALL_PACKET(uri_query, L2_receiver);
-        coap_set_header_uri_query(&req->req_packet, uri_query);        
-        //Set the type of request needed to the working process in order to select the right callback function
-        req->type = TABLE_MISS;
-        
-        //Set payload type and the actual content
-        memcpy(payload, ptr_to_pkt, pkt_dim);
-        coap_set_payload(&req->req_packet, payload, pkt_dim);
-        
+    if(req == NULL){
+        queuebuf_free(q);
+        return;
     }
-    start_request();
+    
+    //Set type of message: CON and POST
+    coap_init_message(&req->req_packet, COAP_TYPE_CON, COAP_POST, 0);
+    //Set the target resource 
+    coap_set_header_uri_path(&req->req_packet, "Flow_engine");
+    //Set the query parameter: "?type=all_packet&tx_mac=<L2 sender's mac address>&rx_mac=<L2 receiver's mac address>"
+    //URI_QUERY_ALL_PACKET(uri_query, L2_sender, L2_receiver);
+    URI_QUERY_ALL_PACKET(uri_query, L2_receiver);
+    coap_set_header_uri_query(&req->req_packet, uri_query);        
+    //Set the type of request needed to the working process in order to select the right callback function
+    req->type = TABLE_MISS;
 
+    //Set payload type and the actual content
+    //memcpy(payload, ptr_to_pkt, pkt_dim);
+    coap_set_payload(&req->req_packet, queuebuf_dataptr(q), queuebuf_datalen(q));
+    start_request();
 }
 
 uint8_t computeNumOfNeighbours(){
@@ -252,7 +256,7 @@ uint8_t prepare_payload_top_update(){
     int16_t rssi;
     //These are the Cbor structures for the first part of the POST payload and for each neighbour node.
     //Where there is 0x00 it means that it is a field which will be set later.
-    uint8_t header[] = {0x84, 0x00, 0x18, 0x00, 0x18, 0x00, 0x00};
+    uint8_t header[] = {0x84, 0x00, 0x19, 0x00, 0x00, 0x18, 0x00, 0x00};
     uint8_t neighbour[] = {0x48, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x82, 0x39, 0x00, 0x00, 0x19, 0x00, 0x00};
     
     //Get the number of neighbours, if it is 0 don't send any update to the Controller then
@@ -261,10 +265,15 @@ uint8_t prepare_payload_top_update(){
         return 0;
     //Set the state information regarding the sending node  
     header[1] = 1;                  //Version of the update structure
-    header[3] = 60;                 //Battery level
-    header[5] = 30;                 //Queue utilization
+    header[3] = 60 << 8;                 //Battery level
+    header[4] = 60 & ((1 << 8) - 1);
+    /*SENSORS_ACTIVATE(battery_sensor);
+    header[3] = battery_sensor.value(0) << 8;                 //Battery level
+    header[4] = battery_sensor.value(0) & ((1 << 8) - 1);
+    SENSORS_DEACTIVATE(battery_sensor);*/
+    header[6] = 30;                 //Queue utilization
     //Set the Cbor major type "Array" with the number of items
-    header[6] = 0xa0 | num_of_neigh;
+    header[7] = 0xa0 | num_of_neigh;
     //Copy the first part of the POST payload
     memcpy(payload, header, sizeof(header));
     payload_dim = sizeof(header);
@@ -318,15 +327,14 @@ static void topology_update(void *ptr){
         if(payload_dim == 0)
             return;
         coap_set_payload(&req->req_packet, payload, payload_dim);
-
-    }
-    start_request();
+        start_request();
+    }    
 }
 
  
 void control_agent_init(){
     ctimer_set(&timer_ttl, TTL_INTERVAL, callback_decrement_ttl, NULL);
-    //ctimer_set(&timer_topology, TOP_UPDATE_PERIOD, topology_update, NULL);
+    ctimer_set(&timer_topology, TOP_UPDATE_PERIOD, topology_update, NULL);
     flowtable_init();
     flowtable_test();
     
@@ -341,12 +349,15 @@ void control_agent_init(){
 
 void client_table_miss_handler(void *response){
   const uint8_t *chunk;
+  queuebuf_free(q);
+  q = NULL;
   if(response == NULL)
       return;
   int len = coap_get_payload(response, &chunk);
   if(len == 0)
       return;
   printf("Table miss: %.*s", len, (char *)chunk);
+  
 }
 
 void client_topology_update_handler(void *response){
